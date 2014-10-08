ba10193282873102be64c926f53f4d27611f79c3
Daniel Schep <dschep@gmail.com>
2014-10-08 14:39:14 -0400

    simpler form

--- v3_mutuallyexclusive_formfields/forms.py
+++ v3_mutuallyexclusive_formfields/forms.py
@@ -4,11 +4,10 @@ from mutuallyexclusive_formfields.forms import (
 
 class TestForm(forms.Form):
     test_field = MutuallyExclusiveValueField(
-            fields=(forms.CharField(), forms.ChoiceField(
-                choices=[('1','1'), ('2','2'), ('3','3')])),
+            fields=(forms.IntegerField(), forms.IntegerField()),
             widget=MutuallyExclusiveRadioWidget(widgets=(
+                forms.Select(choices=[(1,1), (2,2), (3,3)]),
                 forms.TextInput(),
-                forms.Select(choices=[('1','1'), ('2','2'), ('3','3')]),
             )))
     #test_fileurl_field = FileOrURLField(to='file')
 
